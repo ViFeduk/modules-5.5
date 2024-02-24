588445ddda2b64656b2dbe1d03d6febd595dc74d
ViFeduk <viktor2005fedchenkov@mail.ru>
2024-02-24 15:41:02 +0300

    Добавьте файлы проекта.

---
+++ modules 5.3/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace modules_5._3
+{
+    internal class Program
+    {
+        static int num1 = 667;
+        static void Main(string[] args)
+        {
+            var str = Console.ReadLine();
+            Echo(str, 5);
+        }
+        
+
+        static void Echo(string saidworld, int deep)
+        {
+            var modif = saidworld;
+            if (modif.Length > 2)
+            {
+                modif = modif.Remove(0, 2);
+                Console.WriteLine("..." + modif);
+                if (deep > 1)
+                {
+                    Echo(modif, deep - 1);
+                }
+            }
+        }
+    }
+}
